4179425b81b86e74d7718454fd329886b2fb6058
Sasha Herman <sashahermanwebdev@gmail.com>
2024-01-23 19:46:18 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <FriendListSidebar />
     </div>
   );
+}
+
+const FriendListSidebar = () => {
+  return (
+    <div className="sidebar">
+      <ul>
+        {initialFriends.map((el) => (
+          <Friend
+            imageUrl={el.image}
+            name={el.name}
+            balance={el.balance}
+            key={el.id}
+          />
+        ))}
+      </ul>
+      <FormAddFriend />
+      <Button>Add friend</Button>
+    </div>
+  );
+};
+
+const Friend = ({ imageUrl, name, balance }) => {
+  return (
+    <li>
+      <img src={imageUrl} alt={name} />
+      <h3>{name}</h3>
+      {balance > 0 && (
+        <p className="green">
+          {name} owes you R${balance}
+        </p>
+      )}
+      {balance < 0 && (
+        <p className="red">
+          You owe {name} R{Math.abs(balance)}
+        </p>
+      )}
+      {balance === 0 && <p>You and {name} are even</p>}
+      <Button>Select</Button>
+    </li>
+  );
+};
+
+const Button = ({ children }) => {
+  return <button className="button">{children}</button>;
+};
+
+const FormAddFriend = () => {
+  return (
+    <form className="form-add-friend">
+      <label for="friendName">🫂Friend Name</label>
+      <input type="text" id="friendName" />
+
+      <label for="imageUrl">🌄 Image URL</label>
+      <input type="text" id="imageUrl" />
+
+      <Button>Add</Button>
+    </form>
+  );
+};
+
+const FormSplitBill = () => {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with Clark</h2>
+
+      <label for="billValue">💰 Bill value</label>
+      <input type="text" id="billValue" />
+
+      <label for="yourExpense">🕴️ Your expense</label>
+      <input type="text" id="yourExpense" />
+
+      <label for="friendExpense">🧑‍🤝‍🧑 Clark's expense</label>
+      <input type="text" id="friendExpense" />
+
+      <label for="userPaying">🤑 Who is paying the bill?</label>
+      <input type="text" id="userPaying" />
+    </form>
+  )
 }
 
 export default App;
